85236f2d155cfaff4c4d8aa8019132e216137904
Spencer Harston <spencer314@gmail.com>
2018-04-04 13:19:41 -0600

    Added the invoice search window

---
+++ FoodTruck/InvoiceSearch.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FoodTruck
+{
+    /// <summary>
+    /// Interaction logic for InvoiceSearch.xaml
+    /// </summary>
+    public partial class InvoiceSearch : Window
+    {
+        public InvoiceSearch()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles the resetting the form to a default value
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
