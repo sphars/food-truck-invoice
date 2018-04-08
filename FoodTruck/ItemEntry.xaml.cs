4e535106ce956f311b6232c19a845c7d5e6c6187
amyleaur <37940154+amyleaur@users.noreply.github.com>
2018-04-07 21:06:09 -0600

    Commented buttons
    made comments on the item entry buttons and just made descriptions on what they do

--- FoodTruck/ItemEntry.xaml.cs
+++ FoodTruck/ItemEntry.xaml.cs
@@ -23,5 +23,75 @@ namespace FoodTruck
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Add button gives an option to add new items 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddNewButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Edit item button edits the edits in the database 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void EditItemButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// delete button deletes the items in the database
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// ID read only pulls a list that is read only not able to make edits
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void IDreadOnlyButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// The description button pulls up the description 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DescriptionButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Cost button pulls up the cost 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CostButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Save button saves the chages that were made 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
