63c66c37a435287598d18adbe3520118e9c96db2
Rolando Collantes <rolandocollantes@mail.weber.edu>
2018-04-20 22:33:39 -0600

    Fix the prompt when closing in edit mode.
    Also added comments.

--- FoodTruck/Main/MainWindow.xaml.cs
+++ FoodTruck/Main/MainWindow.xaml.cs
@@ -25,7 +25,15 @@ namespace FoodTruck {
         /// </summary>
         private Invoice invoice;
 
+        /// <summary>
+        /// This private field indicates whether the window is creating or modifying an Invoice.
+        /// </summary>
         private bool IsEditMode = false;
+
+        /// <summary>
+        /// This is so EnsureClose() doesn't prompt again.
+        /// </summary>
+        private bool CloseNow;
 
         /// <summary>
         /// Use this constructor for the launch, or for when an invoice IS NOT being provided.
@@ -33,24 +41,34 @@ namespace FoodTruck {
         /// </summary>
         public MainWindow() {
             InitializeComponent();
-
-            ResetForm();
+            this.Closing += OnClosing;
+            ResetWindow();
         }
 
+        /// <summary>
+        /// This constructor loads the MainWindow with a specific invoice.
+        /// Such as when the Search window wants to display or allow the user to edit the invoice.
+        /// </summary>
+        /// <param name="invoice">Invoice object with a proper InvoiceNum pointing to the database.</param>
         public MainWindow(Invoice invoice) : this() {
             this.invoice = invoice;
 
             LoadInvoice();
         }
 
-        private void ResetForm() {
+        /// <summary>
+        /// This method resets the appearance of the window to an initial state.
+        /// </summary>
+        private void ResetWindow() {
             IsEditMode = false;
-            this.Closing -= EnsureClose;
 
             btnClear.IsEnabled = false;
             btnSave.IsEnabled = false;
             btnDeleteInvoice.IsEnabled = false;
             btnEditInvoice.IsEnabled = false;
+            btnAddToInvoice.IsEnabled = false;
+            cbItemList.ItemsSource = null;
+            cbItemList.IsEnabled = false;
 
             btnCreateInvoice.IsEnabled = true;
             lvLineItems.ItemsSource = null;
@@ -62,13 +80,17 @@ namespace FoodTruck {
         }
 
         /// <summary>
-        /// This event handler for the menu item opens an ItemEntry window in dialog mode.
+        /// This event handler for the menu item opens an ItemEntry.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void EditInventoryOnClick(object sender, RoutedEventArgs e) {
-            var window = new ItemEntry();
-            window.ShowDialog();
+            bool closeNow = EnsureClose();
+            if(closeNow) {
+                var window = new ItemEntry();
+                window.Show();
+                this.Close();
+            }
         }
 
         /// <summary>
@@ -77,12 +99,12 @@ namespace FoodTruck {
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void SearchInvoicesOnClick(object sender, RoutedEventArgs e) {
-            // If the window is in edit mode, the event handler would prevent a close.
-            this.Closed += delegate (object s, EventArgs ev) {
+            bool closeNow = EnsureClose();
+            if(closeNow) {
                 var window = new InvoiceSearch();
                 window.Show();
-            };
-            this.Close();
+                this.Close();
+            }
         }
 
         /// <summary>
@@ -106,25 +128,54 @@ namespace FoodTruck {
             btnEditInvoice.IsEnabled = false;
             btnClear.IsEnabled = true;
             btnSave.IsEnabled = true;
-
-            this.Closing += EnsureClose;
+            cbItemList.IsEnabled = true;
         }
 
-        private void EnsureClose(object sender, CancelEventArgs e) {
-            if(IsEditMode) {
-                var result = MessageBox.Show("There are unsaved changes.  Are you sure you want to close this window?",
-                    "Discard changes and close?", MessageBoxButton.OKCancel, MessageBoxImage.Warning);
-                if(result != MessageBoxResult.OK) {
-                    e.Cancel = true;
-                }
+        /// <summary>
+        /// This event handler runs when the user is trying to close the window.  If the window is in edit mode, they're prompted before closing.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnClosing(object sender, CancelEventArgs e) {
+            bool closeNow = EnsureClose();
+            if(!closeNow) {
+                e.Cancel = true;
             }
         }
 
+        /// <summary>
+        /// If the window is in edit mode, prompt the user before losing their work.
+        /// </summary>
+        /// <returns>Returns true if the user wants to close the window, false otherwise.</returns>
+        private bool EnsureClose() {
+            if(CloseNow)
+                return true;
+
+            if(IsEditMode) {
+                var result = MessageBox.Show("There are unsaved changes.  Are you sure you want to close this window?",
+                    "Discard changes and close?", MessageBoxButton.OKCancel, MessageBoxImage.Warning);
+                if(result == MessageBoxResult.OK) {
+                    CloseNow = true;
+                    return true;
+                } else return false;
+            }
+
+            // If not in edit mode, close away.
+            CloseNow = true;
+            return true;
+        }
+
+        /// <summary>
+        /// Creates a new invoice and allows the user to edit it.
+        /// </summary>
         private void CreateInvoice() {
             invoice = new Invoice();
             LoadInvoice();
         }
 
+        /// <summary>
+        /// This enables and shows the controls for the user to edit the Invoice.
+        /// </summary>
         private void LoadInvoice() {
             if(invoice == null) {
                 return;
@@ -139,11 +190,20 @@ namespace FoodTruck {
             LoadItems();
         }
 
+        /// <summary>
+        /// This method fills the ComboBox to allow the user to add LineItems to the invoice.
+        /// </summary>
         private void LoadItems() {
             var items = clsMainLogic.GetAllItemDescs();
             cbItemList.ItemsSource = items;
+            btnAddToInvoice.IsEnabled = true;
         }
 
+        /// <summary>
+        /// This event handler adds 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnAddToInvoice_Click(object sender, RoutedEventArgs e) {
 
         }
