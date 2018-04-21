813a57cf1e8a0756eda69f9b80340117bf3059cd
amyleaur <37940154+amyleaur@users.noreply.github.com>
2018-04-21 10:22:24 -0600

    Added in working data grid for items.

---
+++ FoodTruck/Items/ItemModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodTruck.Items
+{
+    class ItemModel
+    {
+        public string ItemCode { get; set; } = "";
+        public string Desc { get; set; } = "";
+        public Decimal Cost { get; set; } = 0m;
+        public override string ToString()
+        {
+            return $"({ItemCode}, {Cost:C}): {Desc}";
+        }
+
+    }
+}
