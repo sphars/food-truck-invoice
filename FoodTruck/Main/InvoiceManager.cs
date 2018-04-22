715b1ab7d4ed57da0ee07238d5a0d821e004d2ad
Rolando Collantes <rolandocollantes@mail.weber.edu>
2018-04-22 04:13:49 -0600

    Changes to the InvoiceManager
    Added a method to InvoiceManger to calculate the TotalCharge from all the LineItems
    Added a method to remove a LineItem

--- FoodTruck/Main/InvoiceManager.cs
+++ FoodTruck/Main/InvoiceManager.cs
@@ -11,17 +11,45 @@ namespace FoodTruck.Main {
     /// </summary>
     public class InvoiceManager {
 
+        /// <summary>
+        /// This property represents the Invoice that this object is managing.
+        /// </summary>
         public Invoice CurrentInvoice { get; private set; }
 
+        /// <summary>
+        /// This is the list of LineItems belonging to the CurrentInvoice.
+        /// </summary>
         private List<ItemDesc> LineItems;
 
+        /// <summary>
+        /// This constructor for InvoiceManager sets itself up to manage (edit and save) an invoice.
+        /// </summary>
+        /// <param name="invoice">If not null, a preexisting Invoice to modify.</param>
         public InvoiceManager(Invoice invoice) {
-            if(invoice != null) {
-                // Copy of the old invoice so it doesn't overwrite the original in case the user cancels their edits.
-                CurrentInvoice = new Invoice(invoice);
-            } else {
-                // Must be a new invoice.
-                CurrentInvoice = new Invoice();
+            try {
+                if (invoice != null) {
+                    // Copy of the old invoice so it doesn't overwrite the original in case the user cancels their edits.
+                    CurrentInvoice = new Invoice(invoice);
+                } else { // Must be a new invoice.
+                    CurrentInvoice = new Invoice();
+                }
+
+                GetLineItems();
+                CalculateTotal();
+            } catch (Exception) {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// This private method calculates the total cost of all the Line Items.
+        /// Because Items (particularly their costs) can be modified after an Invoice is saved, don't trust the initial TotalCharge.
+        /// </summary>
+        private void CalculateTotal() {
+            try {
+                CurrentInvoice.TotalCharge = LineItems.Sum(li => li.Cost);
+            } catch (Exception) {
+                throw;
             }
         }
 
@@ -30,20 +58,24 @@ namespace FoodTruck.Main {
         /// </summary>
         /// <returns>Returns a List of ItemDesc objects</returns>
         public static List<ItemDesc> GetAllItemDescs() {
-            var dataAccess = new DataAccess();
-            int rows = -1;
-            var dataSet = dataAccess.ExecuteSQLStatement(clsMainSQL.S_IDESC, ref rows);
-            var list = new List<ItemDesc>();
-            if(rows > 0) {
-                foreach(DataRow row in dataSet.Tables[0].Rows) {
-                    list.Add(new ItemDesc() {
-                        ItemCode = (string)row[0],
-                        Desc = (string)row[1],
-                        Cost = (decimal)row[2]
-                    });
+            try {
+                var dataAccess = new DataAccess();
+                int rows = -1;
+                var dataSet = dataAccess.ExecuteSQLStatement(clsMainSQL.S_IDESC, ref rows);
+                var list = new List<ItemDesc>();
+                if (rows > 0) {
+                    foreach (DataRow row in dataSet.Tables[0].Rows) {
+                        list.Add(new ItemDesc() {
+                            ItemCode = (string)row[0],
+                            Desc = (string)row[1],
+                            Cost = (decimal)row[2]
+                        });
+                    }
                 }
+                return list;
+            } catch (Exception) {
+                throw;
             }
-            return list;
         }
 
         /// <summary>
@@ -51,50 +83,58 @@ namespace FoodTruck.Main {
         /// </summary>
         /// <returns>Returns a list of ItemDesc objects, not a list of LineItems</returns>
         public List<ItemDesc> GetLineItems() {
-            // If LineItems has already been populated, return it.
-            if(LineItems != null) {
-                return LineItems;
-            }
+            try {
+                // If LineItems has already been populated, return it.
+                if (LineItems != null) {
+                    return LineItems;
+                }
 
-            // Otherwise, query the database to set it up for the first time:
-            LineItems = new List<ItemDesc>();
+                // Otherwise, query the database to set it up for the first time:
+                LineItems = new List<ItemDesc>();
 
-            if(CurrentInvoice.InvoiceNum != -1) {
-                // First get a list of the LineItems for this invoice.
-                var sql = clsMainSQL.S_LI_P_NUM.Replace("@NUM", CurrentInvoice.InvoiceNum.ToString());
-                var dataAccess = new DataAccess();
-                int rows = -1;
-                var dataSet = dataAccess.ExecuteSQLStatement(sql, ref rows);
-                var LineItemList = new List<LineItem>();
-                if(rows > 0) {
-                    foreach(DataRow row in dataSet.Tables[0].Rows) {
-                        LineItemList.Add(new LineItem() {
-                            InvoiceNum = (int)row[0],
-                            LineItemNum = (int)row[1],
-                            ItemCode = (string)row[2]
-                        });
+                if (CurrentInvoice.InvoiceNum != -1) {
+                    // First get a list of the LineItems for this invoice.
+                    var sql = clsMainSQL.S_LI_P_NUM.Replace("@NUM", CurrentInvoice.InvoiceNum.ToString());
+                    var dataAccess = new DataAccess();
+                    int rows = -1;
+                    var dataSet = dataAccess.ExecuteSQLStatement(sql, ref rows);
+                    var LineItemList = new List<LineItem>();
+                    if (rows > 0) {
+                        foreach (DataRow row in dataSet.Tables[0].Rows) {
+                            LineItemList.Add(new LineItem() {
+                                InvoiceNum = (int)row[0],
+                                LineItemNum = (int)row[1],
+                                ItemCode = (string)row[2]
+                            });
+                        }
+                    }
+
+                    // Get all the ItemDesc
+                    var AllItemDescs = GetAllItemDescs();
+
+                    // For each LineItem, add that ItemDesc to the list.
+                    foreach (var lineItem in LineItemList) {
+                        LineItems.Add(AllItemDescs.First(i => i.ItemCode == lineItem.ItemCode));
                     }
                 }
 
-                // Get all the ItemDesc
-                var AllItemDescs = GetAllItemDescs();
-
-                // For each LineItem, add that ItemDesc to the list.
-                foreach(var lineItem in LineItemList) {
-                    LineItems.Add(AllItemDescs.First(i => i.ItemCode == lineItem.ItemCode));
-                }
+                return LineItems;
+            } catch (Exception) {
+                throw;
             }
-
-            return LineItems;
         }
 
         /// <summary>
         /// Sets the Invoice's InvoiceDate to the specified date.
         /// </summary>
-        /// <param name="date"></param>
+        /// <param name="date">DateTime object representing a date for the invoice.</param>
         public void SetInvoiceDate(DateTime date) {
-            if(date != null) {
-                CurrentInvoice.InvoiceDate = date;
+            try {
+                if (date != null) {
+                    CurrentInvoice.InvoiceDate = date.Date;
+                }
+            } catch (Exception) {
+                throw;
             }
         }
 
@@ -140,7 +180,7 @@ namespace FoodTruck.Main {
                     .Replace("@DATE", CurrentInvoice.InvoiceDate.ToString("MM/dd/yyyy"))
                     .Replace("@TOTAL", $"{CurrentInvoice.TotalCharge}");
                 int invoiceNum = dataAccess.ExecuteInsert(sqlInsert);
-                Console.WriteLine($"Inserted Invoice into DB, got InvoiceNum of: {invoiceNum}");
+
                 CurrentInvoice.InvoiceNum = invoiceNum;
             } else {
                 // Update the invoice that's already there:
@@ -149,14 +189,12 @@ namespace FoodTruck.Main {
                     .Replace("@TOTAL", CurrentInvoice.TotalCharge.ToString())
                     .Replace("@NUM", CurrentInvoice.InvoiceNum.ToString());
                 rowsAffected = dataAccess.ExecuteNonQuery(sqlUpdate);
-                Console.WriteLine($"Updated Invoice with InvoiceNum of {CurrentInvoice.InvoiceNum}, Rows affected: {rowsAffected}");
             }
 
             // Invoice has either been inserted anew or updated.  Erase any pre-existing LineItems:
             // First delete its LineItems:
             var sql = clsMainSQL.D_LI_P_NUM.Replace("@NUM", CurrentInvoice.InvoiceNum.ToString());
             rowsAffected = dataAccess.ExecuteNonQuery(sql);
-            Console.WriteLine($"Deleted {rowsAffected} LineItem rows for InvoiceNum {CurrentInvoice.InvoiceNum}.");
 
             //Now to add all the LineItems:
             int listNum = 1;
@@ -166,7 +204,6 @@ namespace FoodTruck.Main {
                     .Replace("@LNUM", $"{listNum++}")
                     .Replace("@CODE", itemDesc.ItemCode);
                 rowsAffected = dataAccess.ExecuteNonQuery(sqlInsert);
-                Console.WriteLine($"Inserted LineItem {listNum - 1}");
             }
         }
 
@@ -175,12 +212,34 @@ namespace FoodTruck.Main {
         /// </summary>
         /// <param name="item">ItemDesc representing a LineItem.</param>
         public void AddLineItem(ItemDesc item) {
-            if(LineItems == null) {
-                this.GetLineItems();
-            }
+            try {
+                if (LineItems == null) {
+                    this.GetLineItems();
+                }
 
-            LineItems.Add(item);
-            CurrentInvoice.TotalCharge += item.Cost;
+                LineItems.Add(item);
+                CurrentInvoice.TotalCharge += item.Cost;
+            } catch (Exception) {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// This method removes a line item by its index in the LineItems List.
+        /// </summary>
+        /// <param name="index">Index to [LineItems] of the LineItem to remove</param>
+        /// <returns>Returns the LineItems list, just like GetLineItems()</returns>
+        public List<ItemDesc> RemoveLineItem(int index) {
+            if (index < 0 || index >= LineItems.Count)
+                throw new Exception("Index is out of bounds for RemoveLineItem");
+
+            try {
+                CurrentInvoice.TotalCharge -= LineItems[index].Cost;
+                LineItems.RemoveAt(index);
+                return LineItems;
+            } catch (Exception) {
+                throw;
+            }
         }
     }
 }
