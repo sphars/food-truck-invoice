f450ce0e9d97a39b80f1d0e6af47b61d67b70535
Spencer Harston <spencer314@gmail.com>
2018-04-22 20:05:51 -0600

    Add exception handling and comments

--- FoodTruck/Items/ItemEntry.xaml.cs
+++ FoodTruck/Items/ItemEntry.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Reflection;
 
 
 namespace FoodTruck
@@ -12,9 +13,14 @@ namespace FoodTruck
     /// </summary>
     public partial class ItemEntry : Window
     {
-        //Used to access the database
+        /// <summary>
+        /// Used to access the database
+        /// </summary>
         DataAccess db = new DataAccess();
 
+        /// <summary>
+        /// Object to hold a datasource
+        /// </summary>
         public object DataSource { get; set; }
 
         /// <summary>
@@ -22,13 +28,15 @@ namespace FoodTruck
         /// </summary>
         private Invoice storedInvoice;
 
+        /// <summary>
+        /// Constructor for ItemEntry window
+        /// </summary>
+        /// <param name="invoice">The currently selected invoice</param>
         public ItemEntry(Invoice invoice)
         {
-
             InitializeComponent();
             DataGridItemEntry.ItemsSource = clsItemsLogic.GetAllItems();
             storedInvoice = invoice;
-
         }
 
         /// <summary>
@@ -38,16 +46,22 @@ namespace FoodTruck
         /// <param name="e"></param>
         private void AddNewButton_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                //Use the object reference to add new items
+                // clsItemsLogic.InsertItems(ItemCodeBox.Text, ItemDescBox.Text, Convert.ToInt32(CostBox.Text));
+                ItemModel itemModel = new ItemModel();
+                itemModel.ItemCode = ItemCodeBox.Text;
+                itemModel.Desc = ItemDescBox.Text;
+                itemModel.Cost = Decimal.Parse(CostBox.Text);
+                clsItemsLogic.InsertItems(itemModel);
 
-            //Use the object reference to add new items
-            // clsItemsLogic.InsertItems(ItemCodeBox.Text, ItemDescBox.Text, Convert.ToInt32(CostBox.Text));
-            ItemModel itemModel = new ItemModel();
-            itemModel.ItemCode = ItemCodeBox.Text;
-            itemModel.Desc = ItemDescBox.Text;
-            itemModel.Cost = Decimal.Parse(CostBox.Text);
-            clsItemsLogic.InsertItems(itemModel);
-
-            DataGridItemEntry.ItemsSource = clsItemsLogic.GetAllItems();
+                DataGridItemEntry.ItemsSource = clsItemsLogic.GetAllItems();
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name, MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
         }
 
         /// <summary>
@@ -57,14 +71,21 @@ namespace FoodTruck
         /// <param name="e"></param>
         private void EditItemButton_Click(object sender, RoutedEventArgs e)
         {
-            //ItemModel itemModel = (ItemModel)DataGridItemEntry.CurrentItem;
-            ItemModel itemModel = new ItemModel();
-            itemModel.ItemCode = ItemCodeBox.Text;
-            itemModel.Desc = ItemDescBox.Text;
-            itemModel.Cost = Decimal.Parse(CostBox.Text);
+            try
+            {
+                //ItemModel itemModel = (ItemModel)DataGridItemEntry.CurrentItem;
+                ItemModel itemModel = new ItemModel();
+                itemModel.ItemCode = ItemCodeBox.Text;
+                itemModel.Desc = ItemDescBox.Text;
+                itemModel.Cost = Decimal.Parse(CostBox.Text);
 
-            clsItemsLogic.UpdateItem(itemModel);
-            DataGridItemEntry.ItemsSource = clsItemsLogic.GetAllItems();
+                clsItemsLogic.UpdateItem(itemModel);
+                DataGridItemEntry.ItemsSource = clsItemsLogic.GetAllItems();
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name, MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
         }
 
         /// <summary>
@@ -74,21 +95,26 @@ namespace FoodTruck
         /// <param name="e"></param>
         private void DeleteButton_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                //ItemModel itemModel = (ItemModel)DataGridItemEntry.CurrentItem;
+                ItemModel itemModel = new ItemModel();
+                itemModel.ItemCode = ItemCodeBox.Text;
+                itemModel.Desc = ItemDescBox.Text;
+                itemModel.Cost = Decimal.Parse(CostBox.Text);
 
-            //ItemModel itemModel = (ItemModel)DataGridItemEntry.CurrentItem;
-            ItemModel itemModel = new ItemModel();
-            itemModel.ItemCode = ItemCodeBox.Text;
-            itemModel.Desc = ItemDescBox.Text;
-            itemModel.Cost = Decimal.Parse(CostBox.Text);
-
-            clsItemsLogic.DeleteItem(itemModel);
+                clsItemsLogic.DeleteItem(itemModel);
 
 
-            DataGridItemEntry.SelectionChanged -= selectionchanged;
-            DataGridItemEntry.ItemsSource = null;
-            DataGridItemEntry.ItemsSource = clsItemsLogic.GetAllItems();
-            DataGridItemEntry.SelectionChanged += selectionchanged;
-
+                DataGridItemEntry.SelectionChanged -= selectionchanged;
+                DataGridItemEntry.ItemsSource = null;
+                DataGridItemEntry.ItemsSource = clsItemsLogic.GetAllItems();
+                DataGridItemEntry.SelectionChanged += selectionchanged;
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name, MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
         }
 
         /// <summary>
@@ -98,28 +124,58 @@ namespace FoodTruck
         /// <param name="e"></param>
         private void IDreadOnlyButton_Click(object sender, RoutedEventArgs e)
         {
-            DataGridItemEntry.ItemsSource = clsItemsLogic.GetAllItems();
+            try
+            {
+                DataGridItemEntry.ItemsSource = clsItemsLogic.GetAllItems();
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name, MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
         }
 
-
+        /// <summary>
+        /// Handles the changing of items from the datagrid
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void selectionchanged(object sender, SelectionChangedEventArgs e)
         {
-            DataGrid dataGrid = (DataGrid)sender;
-            if (dataGrid.SelectedItem == null)
-                return;
+            try
+            {
+                DataGrid dataGrid = (DataGrid)sender;
+                if (dataGrid.SelectedItem == null)
+                    return;
 
-            ItemModel itemModel = (ItemModel)dataGrid.SelectedItem;
-            ItemCodeBox.Text = itemModel.ItemCode;
-            ItemDescBox.Text = itemModel.Desc;
-            CostBox.Text = itemModel.Cost.ToString();
+                ItemModel itemModel = (ItemModel)dataGrid.SelectedItem;
+                ItemCodeBox.Text = itemModel.ItemCode;
+                ItemDescBox.Text = itemModel.Desc;
+                CostBox.Text = itemModel.Cost.ToString();
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name, MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
 
             // int debug = 0;
         }
 
+        /// <summary>
+        /// Handles closing the item entry window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Close_Item_Entry(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            var window = new MainWindow(storedInvoice);
-            window.Show();
+            try
+            {
+                var window = new MainWindow(storedInvoice);
+                window.Show();
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name, MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
         }
 
         /// <summary>
@@ -129,7 +185,33 @@ namespace FoodTruck
         /// <param name="e"></param>
         private void imgArrowLeft_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            this.Close();
+            try
+            {
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name, MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
         }
+
+        /// <summary>
+        /// exception handler that shows the error
+        /// </summary>
+        /// <param name="sClass">the class</param>
+        /// <param name="sMethod">the method</param>
+        /// <param name="sMessage">the error message</param>
+        private void HandleError(string sClass, string sMethod, string sMessage)
+        {
+            try
+            {
+                MessageBox.Show(sClass + "." + sMethod + " -> " + sMessage);
+            }
+            catch (System.Exception ex)
+            {
+                System.IO.File.AppendAllText("C:\\Error.txt", Environment.NewLine + "HandleError Exception: " + ex.Message);
+            }
+        }
+
     }
 }
